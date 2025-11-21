7c4d2831d7934ae723ea896970831fae1bc9bc3a
zchhh17 <zchhh17@gmail.com>
2025-11-22 04:10:02 +0800

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,15 @@
+# GitHub 访问令牌
+GITHUB_TOKEN="你的PAT"
+
+# GitHub 用户名
+MY_GH_NAME="你的GitHub用户名"
+
+# 仓库前缀
+PREFIX="251121"
+
+# 最大并行线程数
+MAX_THREADS=3
+
+# 脚本完整性检查
+REMOTE_SCRIPT_URL="https://raw.githubusercontent.com/你的GitHub名字/gh-backup-script/main/gh_backup.sh"
+REMOTE_MD5_URL="https://raw.githubusercontent.com/你的GitHub名字/gh-backup-script/main/md5.txt"
