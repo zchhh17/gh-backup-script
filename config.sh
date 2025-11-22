85b101cdf581ae2f1948ebab1aabeac997f3b150
zchhh17 <zchhh17@gmail.com>
2025-11-22 12:29:40 +0800

    Add GitHub configuration variables to config.sh
    Added configuration variables for GitHub access.

--- config.sh
+++ config.sh
@@ -1,2 +1,14 @@
+# GitHub 访问令牌
+GITHUB_TOKEN=""
+
+# GitHub 用户名
+MY_GH_NAME="zchhh17"
+
+# 仓库前缀
+PREFIX="251121"
+
+# 最大并行线程数
+MAX_THREADS=3
+
 REMOTE_SCRIPT_URL="https://raw.githubusercontent.com/zchhh17/gh-backup-script/main/gh_backup5.sh"
 REMOTE_MD5_URL="https://raw.githubusercontent.com/zchhh17/gh-backup-script/main/md5.txt"
